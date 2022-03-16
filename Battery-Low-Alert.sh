87430555e9e83adadc9928b334eea83ba27f3b38
Peter Finn <c16407722@mydit.ie>
2022-03-16 00:40:45 +0000

    Update Battery-Low-Alert.sh

--- Battery-Low-Alert.sh
+++ Battery-Low-Alert.sh
@@ -1,2 +1,2 @@
-while true; do p=$(acpi|awk '{print $4}'|grep -o "^.."); if [ $p -lt 20 ]; then speak "Battery Low"; fi;sleep 5; done
+while true; do p=$(acpi|awk '{print $4}'|grep -o "^.."); [ $p -lt 30 ] && (s=$(acpi|tr -d ','|awk '{print $3}'); [ "$s" = "Discharging" ] && espeak "Battery Low"); sleep 5; done
 # espeak is necessary. 20 is the battery % to trigger alert
